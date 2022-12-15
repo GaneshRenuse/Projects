47d16dca1cf1a7fbe89667e4f5cf701c8dd26a78
coding sigma <92455602+GaneshRenuse@users.noreply.github.com>
2022-12-15 13:35:13 +0000

    modified file added funtions

--- library_management/Library_management.c
+++ library_management/Library_management.c
@@ -1,46 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // create a structure to save books data
 struct books
 {
     int id;
-    char* name;
-}b[9];
+    char *name;
+} b[9];
 
 void bookdetails(struct books[]);
+void save_tofile(struct books[]);
+void admin_login();
+void student_login();
 
-int main()  
+int main()
 {
-    // open file using file pointer
-    FILE *fptr;
-    fptr = fopen("program.txt","w");
-
-    //if there is error in file opening it returns null 
-    if(fptr == NULL)
-    {
-      printf("Error!");   
-      exit(1);             
-    }
-
     // call bookdetails funtion to set book details
     bookdetails(b);
 
-    for (int i = 0; i < 10; i++)
+    int choice;
+
+    while (1)
     {
-        //print book details to file
-        fprintf(fptr,"%d\t%s\n", b[i].id, b[i].name);
+        system("clr");
+        printf("\n- - - - - AG LIBRARY - - - - -\n");
+        printf("\t\t1] Admin Login\n");
+        printf("\t\t2] Student Login\n");
+        printf("\t\t3] Exit\n");
+        printf("\t\tEnter --> ");
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+        case 1:
+            admin_login();
+            break;
+        case 2:
+            //student_login();
+            break;
+        case 3:
+            exit(1);
+            break;
+        default:
+            printf("\n\n");
+            break;
+        }
     }
-    
-    // close file pointer.
-    fclose(fptr);
 
     return 0;
 }
 
+void admin_login()
+{
+    int chk = 0;
+    char pass[8] = "ad2022li";
+    char chkpass[8];
+
+    printf("Enter password : ");
+    scanf("%s", chkpass);
+
+    if (strcmp(chkpass, pass))
+    {
+        printf("access granted.\n");
+        chk = 1;
+    }
+    else
+    {
+        printf("access denied.\n");
+        chk = 0;
+    }
+}
+
 void bookdetails(struct books b[9])
 {
-    //set book details
+    // set book details
     for (int i = 0; i <= 9; i++)
     {
         b[i].id = i + 101;
@@ -56,4 +90,27 @@ void bookdetails(struct books b[9])
     b[7].name = "java";
     b[8].name = "python";
     b[9].name = "javascript";
+}
+
+void save_tofile(struct books b[9])
+{
+    // open file using file pointer
+    FILE *fptr;
+    fptr = fopen("program.txt", "w");
+
+    // if there is error in file opening it returns null
+    if (fptr == NULL)
+    {
+        printf("Error!");
+        exit(1);
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        // print book details to file
+        fprintf(fptr, "%d\t%s\n", b[i].id, b[i].name);
+    }
+
+    // close file pointer.
+    fclose(fptr);
 }
